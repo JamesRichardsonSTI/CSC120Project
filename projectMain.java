5785eff543e993e9af0899646f26b84eccc6aa3d
JamesRichardsonSTI <JamesRichardsonSTI@users.noreply.github.com>
2021-03-11 14:36:08 -0500

    Finished up response and rest of code

--- projectMain.java
+++ projectMain.java
@@ -13,6 +13,8 @@
     double gpu;
     double clockSpeed;  
     double coreNum;  
+    double chosenValue;
+    double finScore;
     
          String str;
          
@@ -33,9 +35,53 @@
         JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
         
         null, options, options[0]);
-        
-   
+        double mut;
+       if(response == 0){
+      
+       }else
+       if(response == 1){
+   mut = .75;
+       }else
+       if(response == 2){
+    mut = .55;
+       }else   
+       if(response == 3){
+       mut = .35;
+       
+       }
+ mut = 1;
+ finScore =((5*gpu)+(coreNum*clockSpeed)*mut);
  
+ if(finScore >= 17000){
+ JOptionPane.showMessageDialog(null,"Ultra");
+ 
+ }else{
+ 
+ 
+ if(15000 <= finScore && finScore <= 17000){
+ JOptionPane.showMessageDialog(null,"High");
+ 
+ }else{
+ 
+ if(13000< finScore && finScore <= 15000){
+ JOptionPane.showMessageDialog(null,"Medium");
+ 
+ }
+ 
+ }
+ if(11000< finScore && finScore <= 13000){
+ JOptionPane.showMessageDialog(null,"Low");
+ 
+ }else{
+ 
+ if(finScore <= 11000){
+JOptionPane.showMessageDialog(null,"Unable to Play");
+ 
+ }
+ 
+ }
+ 
+ }
     
      }
   }
